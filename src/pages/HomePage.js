eb9d99e59c637a9c1ab972fa2267e448cc1dc800
HoangAnhNguyen0710 <vipboy69798999@gmail.com>
2022-07-14 10:54:05 +0700

    update homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -44,8 +44,8 @@ const HomePage = () => {
   const HomeContent = (
     <>
       <div className="min-h-fit py-8 box-border">
-        <div className="w-full my-8 relative md:h-fit">
-        <div className="bg-gradient-to-t from-slate-50 to-black absolute h-full w-full top-0 left-0 z-10 opacity-40"></div>
+        <div className="md:mx-10 md:w-11/12 my-8 relative md:h-fit">
+        <div className=" bg-gradient-to-t from-slate-50 to-black absolute h-full w-full top-0 left-0 z-10 opacity-40"></div>
         <div className="">
         <Slider {...settings}>
           <div className="">
@@ -65,23 +65,27 @@ const HomePage = () => {
       <div className="flex flex-wrap justify-center md:flex-nowrap p-6">
         <div className="w-4/5 md:w-1/3 lg:w-1/3 p-3 m-3 shadow-lg min-h-fit flex flex-col items-center justify-between">
           <span className="text-xl font-medium">Kho tài liệu, đề thi phong phú</span>
-          <img alt="" src={document} className='w-3/5'/>
-          <NavLink to='/' className=" p-3 bg-main text-white rounded-3xl">Khám phá</NavLink>
+          <img alt="" src={document} className='w-1/2 p-4'/>
+          <NavLink to='/documents' className=" p-3 bg-main text-white rounded-3xl">Khám phá</NavLink>
         </div>
         <div className="w-4/5 md:w-1/3 lg:w-1/3 p-3 m-3 shadow-lg min-h-fit flex flex-col items-center justify-between">
           <span className="text-xl font-medium">Review môn học từ những người đi trước</span>
-          <img alt="" src={review} className='w-3/5'/>
-          <NavLink to='/' className=" p-3 bg-main text-white rounded-3xl">Khám phá</NavLink>
+          <img alt="" src={review} className='w-1/2 p-3 pb-4'/>
+          <NavLink to='/reviews' className=" p-3 bg-main text-white rounded-3xl">Khám phá</NavLink>
         </div>
         <div className="w-4/5 md:w-1/3 lg:w-1/3 p-3 m-3 shadow-lg min-h-fit flex flex-col items-center justify-between">
           <span className="text-xl font-medium">Giải đáp vướng bận cùng các bậc tiền bối</span>
-          <img alt="" src={qa} className='w-3/5'/>
-          <NavLink to='/' className=" p-3 bg-main text-white rounded-3xl">Khám phá</NavLink>
+          <img alt="" src={qa} className='w-1/2 p-3'/>
+          <NavLink to='/questions' className=" p-3 bg-main text-white rounded-3xl">Khám phá</NavLink>
         </div>
       </div>
       </div>
-      <div className="p-6 px-9 text-3xl font-bold">TRENDING (TÀI LIỆU ĐƯỢC TÌM KIẾM NHIỀU NHẤT)</div> 
+      <div className="p-6 px-9">
+      <div className="text-3xl font-bold">TRENDING (TÀI LIỆU ĐƯỢC TÌM KIẾM NHIỀU NHẤT)</div> 
       <DocumentList filter="trending"/>
+      <div className="text-3xl font-bold">BEST DOCS (TÀI LIỆU ĐƯỢC ĐÁNH GIÁ CAO NHẤT)</div> 
+      <DocumentList filter="best"/>
+      </div>
     </>
   );
 
